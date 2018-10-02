d3eb8143efaa3778e017def53a899e1fbc4f561d
dmitry_akimenko <dmitry_akimenko>
2018-10-02 13:32:25 +0300

    added some components and assets

---
+++ src/app/public/public.component.ts
@@ -0,0 +1,15 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public',
+  templateUrl: './public.component.html',
+})
+export class PublicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
